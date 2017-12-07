7faada6e8368ac9a71edcf8f82b9116af2dbb9aa
zhouH0901 <zhezhou@my.bridgeport.edu>
2017-12-07 03:27:09 -0500

    add component in U project to do inital action

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ReceipesComponent } from './receipes/receipes.component';
+import { RecipeListComponent } from './receipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './receipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './receipes/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ReceipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
