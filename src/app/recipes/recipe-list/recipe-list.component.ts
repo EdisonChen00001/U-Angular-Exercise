f43ac25b8dee6723cb3632808b9e7e677ea003b7
zhouH0901 <zhezhou@my.bridgeport.edu>
2017-12-10 23:24:47 -0500

    add content to Recipes Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes:Recipes[] = [
+  recipes:Recipe[] = [
     new Recipe('A Test Recipe','This is simply a test','http://maxpixel.freegreatpicture.com/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg')
   ];
 
